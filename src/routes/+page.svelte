aa778cafb94329e6e0d95b4ab853b90dacc3e31a
Simon Amby Pedersen <yspr@novonordisk.com>
2025-06-16 10:10:00 +0200

    Added tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+ import * as Tabs from "$lib/components/ui/tabs/index.js";
+</script>
+ 
+<div class="flex w-full max-w-sm flex-col gap-6 justify-center items-center mx-auto">
+ <Tabs.Root value="fredag">
+    <Tabs.List>
+     <Tabs.Trigger value="friday">Fredag 20/6</Tabs.Trigger>
+     <Tabs.Trigger value="saturday">Lørdag 21/6</Tabs.Trigger>
+     <Tabs.Trigger value="sunday">Søndag 22/6</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="friday">
+    </Tabs.Content>
+    <Tabs.Content value="saturday">
+    </Tabs.Content>
+    <Tabs.Content value="sunday">
+    </Tabs.Content>
+ </Tabs.Root>
+</div>
