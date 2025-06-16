0de2a0e7f1b5b4d7dd3532e337fa7153c2a41959
Simon Amby Pedersen <yspr@novonordisk.com>
2025-06-16 10:17:52 +0200

    Added Table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
- import * as Tabs from "$lib/components/ui/tabs/index.js";
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
 </script>
- 
-<div class="flex w-full max-w-sm flex-col gap-6 justify-center items-center mx-auto">
- <Tabs.Root value="fredag">
-    <Tabs.List>
-     <Tabs.Trigger value="friday">Fredag 20/6</Tabs.Trigger>
-     <Tabs.Trigger value="saturday">Lørdag 21/6</Tabs.Trigger>
-     <Tabs.Trigger value="sunday">Søndag 22/6</Tabs.Trigger>
-    </Tabs.List>
-    <Tabs.Content value="friday">
-    </Tabs.Content>
-    <Tabs.Content value="saturday">
-    </Tabs.Content>
-    <Tabs.Content value="sunday">
-    </Tabs.Content>
- </Tabs.Root>
+
+<div class="mx-auto my-5 flex w-full max-w-sm flex-col items-center justify-center gap-6">
+	<Tabs.Root value="friday">
+		<Tabs.List>
+			<Tabs.Trigger value="friday">Fredag 20/6</Tabs.Trigger>
+			<Tabs.Trigger value="saturday">Lørdag 21/6</Tabs.Trigger>
+			<Tabs.Trigger value="sunday">Søndag 22/6</Tabs.Trigger>
+		</Tabs.List>
+		<Tabs.Content value="friday"></Tabs.Content>
+		<Tabs.Content value="saturday"></Tabs.Content>
+		<Tabs.Content value="sunday"></Tabs.Content>
+	</Tabs.Root>
 </div>
